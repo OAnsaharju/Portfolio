6375d168c6d316be0e0e94b48764f8fd843c7a10
OAnsaharju <otto24002@student.hamk.fi>
2024-09-23 17:23:16 +0300

    Updated Golfapp demo link

--- src/components/demo.js
+++ src/components/demo.js
@@ -11,9 +11,7 @@ function Demo() {
         <div className="demo-item">
           <h4 className="demo-label">Golf App</h4>
           <p className="demo-text">{t("demo.demo-text-1")}</p>
-          <a href="https://6641011cbcb7cefbfe800c1c--teal-mermaid-281a02.netlify.app/">
-            Demo
-          </a>
+          <a href="https://teal-mermaid-281a02.netlify.app/">Demo</a>
 
           <a href="https://github.com/OAnsaharju/Golfapp">
             {t("demo.repo-text")}
